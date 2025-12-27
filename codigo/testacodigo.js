ba3dc89a93cc919843fbb5a3e4c66494151363ae
Jamilly San <jamiilly.gabriiellly@gmail.com>
2025-12-27 08:50:47 -0300

    Projetos IFMG

---
+++ codigo/testacodigo.js
@@ -0,0 +1,19 @@
+/*for(let i = 1 ; i < 20;i++ ){
+console.log(i)
+}*/
+//let frutas =['Bananas','Maçã','Melancia'];
+//for(let i = 1 ; i < 11;i++ ){
+//console.log(frutas[1]);
+//}
+
+/*for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+}*/
+/*for(let index = 5; index >= 1 ;index = index-1){
+    console.log(index)
+}
+console.log('feliz natal!!!!')*/
+/*let numero = 7;
+for( let contador = 1;contador <= 9;contador   += 1) {
+    console.log(numero*contador);
+}*/
